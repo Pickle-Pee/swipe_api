61101e1282c2152b37eb0d78f403fb1bea4838d1
Pickle-Pee <kirillkas5642013@gmail.com>
2023-09-14 13:45:54 +0300

    add socket

--- main_app/app.py
+++ main_app/app.py
@@ -9,11 +9,9 @@ from controllers.likes_controller import router as likes_router
 from controllers.subscription_controller import router as subscription_router
 from controllers.matches_controller import router as matches_router
 from controllers.communication_controller import router as communication_router
-from common.utils.socket_utils import sio, socket_app
 
 app = FastAPI()
 
-app.mount("/socket.io", socket_app)
 app.include_router(auth_router)
 app.include_router(user_router)
 app.include_router(interests_router)
@@ -22,10 +20,9 @@ app.include_router(subscription_router)
 app.include_router(matches_router)
 app.include_router(communication_router)
 
-socket_app = socketio.ASGIApp(socketio_server=sio, other_asgi_app=app)
 
 if __name__ == "__main__":
     import uvicorn
 
     app_host = os.getenv("MAIN_APP_HOST")
-    uvicorn.run(socket_app, host=app_host, port=1024)
+    uvicorn.run(app, host=app_host, port=1024)
--- socket_app/app.py
+++ socket_app/app.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI, Depends
+import os
 import socketio
-
 from common.models.communication_models import Chat, Message
 from common.utils.auth_utils import get_user_id_from_token
 from config import SECRET_KEY, SessionLocal
@@ -11,23 +10,30 @@ socket_app = socketio.ASGIApp(sio)
 connected_users = {}
 
 
-@sio.event
-async def connect(sid, environ):
-    access_token = environ.get('HTTP_AUTHORIZATION')  # Получаем токен из заголовков
-    print(f"Received token: {access_token}")
-    if not access_token:
-        return False  # Закрыть соединение, если токена нет
-
+async def authenticate_user(access_token, environ):
     try:
         user_id = get_user_id_from_token(access_token, SECRET_KEY)
-        print(f"Authenticated user_id: {user_id}")  # Логирование аутентифицированного user_id
+        print(f"Authenticated user_id: {user_id}")
+        return user_id
     except Exception as e:
         print(f"Failed to authenticate user. Exception: {type(e).__name__}, Message: {str(e)}")
-        print(f"Failed token: {access_token[:10]}...")  # Логируем первые 10 символов токена для безопасности
-        print(f"Environ: {environ}")  # Логируем дополнительные сведения о запросе (опционально)
+        print(f"Failed token: {access_token[:10]}...")
+        print(f"Environ: {environ}")
+        return None
+
+
+@sio.event
+async def connect(sid, environ):
+    access_token = environ.get('HTTP_AUTHORIZATION')
+    print(f"Received token: {access_token}")
+    if not access_token:
         return False
 
-    connected_users[user_id] = sid  # Сохраняем sid пользователя
+    user_id = await authenticate_user(access_token, environ)
+    if user_id is None:
+        return False
+
+    connected_users[user_id] = sid
 
 
 @sio.event
@@ -36,7 +42,6 @@ async def send_message(sid, data):
     receiver_id = data.get('receiver_id')
     message_content = data.get('message')
 
-    # Получаем ID чата между sender и receiver
     with SessionLocal() as db:
         chat = db.query(Chat).filter(
             ((Chat.user1_id == sender_id) & (Chat.user2_id == receiver_id)) |
@@ -44,7 +49,6 @@ async def send_message(sid, data):
         ).first()
 
         if not chat:
-            # Если чата нет, создаем новый (или вы можете выбросить ошибку)
             new_chat = Chat(user1_id=sender_id, user2_id=receiver_id)
             db.add(new_chat)
             db.commit()
@@ -52,12 +56,10 @@ async def send_message(sid, data):
         else:
             chat_id = chat.id
 
-        # Сохраняем сообщение в базе данных
         new_message = Message(chat_id=chat_id, sender_id=sender_id, content=message_content)
         db.add(new_message)
         db.commit()
 
-    # Отправляем сообщение получателю, если он онлайн
     receiver_sid = connected_users.get(receiver_id)
     if receiver_sid:
         await sio.emit('new_message', {'message': message_content}, room=receiver_sid)
@@ -68,3 +70,10 @@ async def disconnect(sid):
     user_id = [k for k, v in connected_users.items() if v == sid][0]
     print(f"Disconnected: {user_id}")
     del connected_users[user_id]
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    app_host = os.getenv("MAIN_APP_HOST")
+    uvicorn.run(socket_app, host=app_host, port=1025)
