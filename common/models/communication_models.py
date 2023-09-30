7236721eb0b5535f1d8391154550cf1e0feb64cc
Pickle-Pee <kirillkas5642013@gmail.com>
2023-09-30 23:28:55 +0300

    add remove user

--- common/models/communication_models.py
+++ common/models/communication_models.py
@@ -16,8 +16,8 @@ class Chat(Base):
     __tablename__ = 'chats'
 
     id = Column(Integer, primary_key=True, index=True)
-    user1_id = Column(Integer, ForeignKey('users.id'))
-    user2_id = Column(Integer, ForeignKey('users.id'))
+    user1_id = Column(Integer, ForeignKey('users.id'), nullable=True)
+    user2_id = Column(Integer, ForeignKey('users.id'), nullable=True)
     created_at = Column(DateTime, default=datetime.utcnow)
 
     messages = relationship("Message", foreign_keys="[Message.chat_id]")
--- common/utils/crud.py
+++ common/utils/crud.py
@@ -8,21 +8,21 @@ from common.models.likes_models import Dislike, Favorite, Like
 from common.models.user_models import User
 
 
-def delete_user_and_related_data(db: Session, user_id: int):
-    db.query(RefreshToken).filter(RefreshToken.user_id == user_id).delete()
-    db.query(Dislike).filter(Dislike.user_id == user_id).delete()
-    db.query(Favorite).filter(Favorite.user_id == user_id).delete()
-    db.query(Like).filter(Like.user_id == user_id).delete()
-    db.query(UserInterest).filter(UserInterest.user_id == user_id).delete()
-    db.query(Chat).filter(Chat.user1_id == user_id).delete()
-    db.query(Message).filter(
-        Message.chat_id.in_(
-            db.query(Chat.id).filter(or_(Chat.user1_id == user_id, Chat.user2_id == user_id))
-        )
-    ).delete(synchronize_session='fetch')
-    db.query(Message).filter(Message.sender_id == user_id).delete()
-    user = db.query(User).filter(User.id == user_id).first()
+def delete_user_and_related_data(db: Session, user_id: int) -> bool:
+    # Удаление данных пользователя, но сохранение сообщений и чатов
+    db.query(RefreshToken).filter(RefreshToken.user_id == user_id).delete(synchronize_session=False)
+    db.query(Dislike).filter(Dislike.user_id == user_id).delete(synchronize_session=False)
+    db.query(Favorite).filter(Favorite.user_id == user_id).delete(synchronize_session=False)
+    db.query(Like).filter(Like.user_id == user_id).delete(synchronize_session=False)
+    db.query(UserInterest).filter(UserInterest.user_id == user_id).delete(synchronize_session=False)
 
+    # Заменяем user_id на None или ID специального пользователя в чатах
+    db.query(Chat).filter(Chat.user1_id == user_id).update({Chat.user1_id: None})
+    db.query(Chat).filter(Chat.user2_id == user_id).update({Chat.user2_id: None})
+    db.commit()
+
+    # Удаление самого пользователя
+    user = db.query(User).filter(User.id == user_id).first()
     if user:
         db.delete(user)
         db.commit()
