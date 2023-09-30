205802e11ce663f910110fac7c575a2cd62c64a5
Pickle-Pee <kirillkas5642013@gmail.com>
2023-09-30 23:51:21 +0300

    add remove chats && messages

--- common/utils/crud.py
+++ common/utils/crud.py
@@ -2,7 +2,7 @@ from sqlalchemy import or_
 from sqlalchemy.orm import Session
 
 from common.models.auth_models import RefreshToken
-from common.models.communication_models import Chat, Message
+from common.models.communication_models import Chat, Message, Media
 from common.models.interests_models import UserInterest
 from common.models.likes_models import Dislike, Favorite, Like
 from common.models.user_models import User
@@ -27,4 +27,32 @@ def delete_user_and_related_data(db: Session, user_id: int) -> bool:
         db.delete(user)
         db.commit()
         return True
+    return False
+
+
+def delete_chat_and_related_messages(db: Session, chat_id: int):
+    messages = db.query(Message.id).filter(Message.chat_id == chat_id).all()
+    message_ids = [message[0] for message in messages]  # Изменение здесь
+
+    if message_ids:
+        db.query(Media).filter(Media.message_id.in_(message_ids)).delete(synchronize_session='fetch')
+        db.query(Message).filter(Message.id.in_(message_ids)).delete(synchronize_session='fetch')
+
+    chat = db.query(Chat).filter(Chat.id == chat_id).first()
+    if chat:
+        db.delete(chat)
+        db.commit()
+        return True
+
+    return False
+
+
+def delete_message_and_related_media(db: Session, message_id: int):
+    db.query(Media).filter(Media.message_id == message_id).delete(synchronize_session='fetch')
+    message = db.query(Message).filter(Message.id == message_id).first()
+    if message:
+        db.delete(message)
+        db.commit()
+        return True
+
     return False
