97c0736b96b556cd67037c7976201d63e51f0281
Pickle-Pee <kirillkas5642013@gmail.com>
2023-10-08 18:10:35 +0300

    configurate pushes

--- push_app/app.py
+++ push_app/app.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel
 import firebase_admin
 from firebase_admin import messaging, credentials
 from config import FIREBASE_CREDENTIALS_PATH
+import os
+
 
 app = FastAPI()
 
@@ -31,3 +33,11 @@ async def send_push(msg: PushMessage):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail={"success": False, "error": str(e)})
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    app_host = os.getenv("MAIN_APP_HOST")
+    uvicorn.run(app, host=app_host, port=1026)
+
