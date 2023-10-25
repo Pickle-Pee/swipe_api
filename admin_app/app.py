6257d6cc5a01709ccba92a796b59598fcebdf695
Pickle-Pee <kirillkas5642013@gmail.com>
2023-10-25 12:07:54 +0300

    test cd

--- admin_app/app.py
+++ admin_app/app.py
@@ -26,6 +26,22 @@ async def login_admin(form_data: OAuth2PasswordRequestForm = Depends()):
         return {"access_token": access_token, "token_type": "bearer"}
 
 
+@app.post("/admin/all_users", summary="Авторизация администратора")
+async def login_admin(form_data: OAuth2PasswordRequestForm = Depends()):
+    with SessionLocal() as db:
+        admin = get_admin_by_username(db, username=form_data.username)
+
+        if not admin or not security.verify_password(form_data.password, admin.hashed_password):
+            raise HTTPException(
+                status_code=401,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+
+        access_token = security.create_access_token(data={"sub": admin.username, "admin_id": admin.id})
+        return {"access_token": access_token, "token_type": "bearer"}
+
+
 
 if __name__ == "__main__":
     import uvicorn
